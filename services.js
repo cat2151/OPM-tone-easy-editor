7c384a7f747438dc8d192fa588b1447d3ee3e06d
cat2151 <fct_2004@mail.goo.ne.jp>
2016-03-22 17:54:22 +0900

    MML生成結果テキストボックスでの数値直接エディットをする際にわかりやすいよう、パラメータのコメントも出力するようにした

--- services.js
+++ services.js
@@ -7,24 +7,31 @@ function() {
     s += p.alg + ',';
     s += p.fb + ',';
     s += '\n';
+    var OPM_COMMENT = '/*AR DR  SR  RR  SL  TL  KS MUL DT1 DT2 AMS*/' + '\n';
+    s += OPM_COMMENT;
     var i;
     for (i = 0; i < 4; i++) {
-      s += p['op' + i + 'ar'] + ',';
-      s += p['op' + i + 'dr'] + ',';
-      s += p['op' + i + 'sr'] + ',';
-      s += p['op' + i + 'rr'] + ',';
-      s += p['op' + i + 'sl'] + ',';
-      s += p['op' + i + 'tl'] + ',';
-      s += p['op' + i + 'ks'] + ',';
-      s += p['op' + i + 'mul'] + ',';
-      s += p['op' + i + 'dt1'] + ',';
-      s += p['op' + i + 'dt2'] + ',';
-      s += p['op' + i + 'ams']// + ',';
+      s += pd(p['op' + i + 'ar']) + ',';
+      s += pd(p['op' + i + 'dr']) + ',';
+      s += pd(p['op' + i + 'sr']) + ',';
+      s += pd(p['op' + i + 'rr']) + ',';
+      s += pd(p['op' + i + 'sl']) + ',';
+      s += pd(p['op' + i + 'tl']) + ',';
+      s += pd(p['op' + i + 'ks']) + ',';
+      s += pd(p['op' + i + 'mul']) + ',';
+      s += pd(p['op' + i + 'dt1']) + ',';
+      s += pd(p['op' + i + 'dt2']) + ',';
+      s += pd(p['op' + i + 'ams'])// + ',';
       s += '\n';
     }
     s += '};';
     s += '%6@0' + p.previewPhrase;
     return s;
+  }
+
+  function pd(i) { // padding(¶–„‚ßA”¼Špspace)
+   var s = "  " + String(i);
+   return s.substr(s.length - 3);
   }
 
   return {
